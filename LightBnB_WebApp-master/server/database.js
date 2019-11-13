3294897dd3ce2fede800ccad7e7e282d72e770f1
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-11-13 02:34:40 +0000

    Link login, signup and logout sql database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,13 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const {Pool} = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 
 /// Users
 
@@ -9,16 +17,13 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query(`
+  SELECT * FROM users
+  WHERE email = $1
+  `,[`${email}`])
+  .then(res => {
+    res.rows[0];
+  })
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -28,7 +33,13 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+  SELECT * FROM users
+  WHERE id = $1
+  `,[`${id}`])
+  .then(res => {
+    res.rows[0];
+  })
 }
 exports.getUserWithId = getUserWithId;
 
@@ -39,10 +50,14 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(`
+  INSERT INTO users(name, email, password)
+  VALUES('${user.name}', '${user.email}', '${user.password}')
+  RETURNING *;
+  `)
+  .then(res => {
+    res.rows;
+  })
 }
 exports.addUser = addUser;
 
@@ -67,11 +82,20 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  // const limitedProperties = {};
+  // for (let i = 1; i <= limit; i++) {
+  //   limitedProperties[i] = properties[i];
+  // }
+  // return Promise.resolve(limitedProperties);
+  return pool.query(`
+  SELECT properties.*, AVG(rating) as average_rating 
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  GROUP BY properties.id
+  LIMIT $1
+  `,[limit])
+  .then(res => res.rows);
+  
 }
 exports.getAllProperties = getAllProperties;
 
